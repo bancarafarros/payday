3dfb9378a2a9c3ce91adfc48bdc7b0d1e2f7ca10
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-08-09 22:34:01 +0700

    partisi view dashboard admin

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -3,7 +3,10 @@
 class Dashboard extends CI_Controller
 {
     function index() {
+        $this->load->view('admin/header');
+        $this->load->view('admin/sidebar');
         $this->load->view('admin/dashboard');
+        $this->load->view('admin/footer');
     }
     
 }
