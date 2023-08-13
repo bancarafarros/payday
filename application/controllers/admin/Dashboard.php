271418f6697fb5532b244654ccfb2fdce99064da
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-08-13 22:53:33 +0700

    add title tab

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -3,7 +3,9 @@
 class Dashboard extends CI_Controller
 {
     function index() {
-        $this->load->view('templates_admin/header');
+        $data['title'] = 'Dashboard';
+        
+        $this->load->view('templates_admin/header', $data);
         $this->load->view('templates_admin/sidebar');
         $this->load->view('admin/dashboard');
         $this->load->view('templates_admin/footer');
