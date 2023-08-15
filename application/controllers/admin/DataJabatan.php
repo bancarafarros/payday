1d1a3a059713d7e0579d38cc394ab35c57d0c2fd
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-08-15 20:15:25 +0700

    add function createJabatan

--- application/controllers/admin/DataJabatan.php
+++ application/controllers/admin/DataJabatan.php
@@ -20,7 +20,14 @@ class DataJabatan extends CI_Controller
         $this->load->view('templates_admin/footer');
     }
 
-    
+    public function createJabatan() {
+        $data['title'] = 'Data Jabatan';
+
+        $this->load->view('templates_admin/header', $data);
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/data_jabatan', $data);
+        $this->load->view('templates_admin/footer');
+    }
 }
 
 ?>
