de146cfd13f848796acb7d3056a4ba30ea45505a
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-08-15 21:58:46 +0700

    add add function createJabatan

--- application/models/M_penggajian.php
+++ application/models/M_penggajian.php
@@ -5,6 +5,10 @@ class M_penggajian extends CI_Model
     public function getData($table) {
         return $this->db->get($table);
     }
+    
+    public function createJabatan($data, $table) {
+        $this->db->insert($table, $data);
+    }
 }
 
 ?>
