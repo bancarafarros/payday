f41a399ed832b32e7c5702dcd40b775a10bdcbd6
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-08-15 21:33:31 +0700

    add rules form_validation

--- application/controllers/admin/DataJabatan.php
+++ application/controllers/admin/DataJabatan.php
@@ -7,6 +7,13 @@ class DataJabatan extends CI_Controller
 	// 	parent::__construct();
 	// 	$this->load->model('M_penggajian', 'penggajian');
 	// }
+
+    public function _rules() {
+        $this->form_validation->set_rules('nama_jabatan', 'nama jabatan', 'required');
+        $this->form_validation->set_rules('gaji_pokok', 'gaji pokok', 'required');
+        $this->form_validation->set_rules('tj_transport', 'tunjanagn tarnsport', 'required');
+        $this->form_validation->set_rules('uang_makan', 'uang makan', 'required');
+    }
     
     public function index() {
         $this->load->model('M_penggajian', 'penggajian');
