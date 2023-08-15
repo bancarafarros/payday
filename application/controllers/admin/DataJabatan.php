4fb8998e33e20d40fa0d7d82c0b9873d6e7875e8
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-08-15 20:21:40 +0700

    ganti url view data-jabatan

--- application/controllers/admin/DataJabatan.php
+++ application/controllers/admin/DataJabatan.php
@@ -16,7 +16,7 @@ class DataJabatan extends CI_Controller
 
         $this->load->view('templates_admin/header', $data);
         $this->load->view('templates_admin/sidebar');
-        $this->load->view('admin/data_jabatan', $data);
+        $this->load->view('admin/data-jabatan', $data);
         $this->load->view('templates_admin/footer');
     }
 
