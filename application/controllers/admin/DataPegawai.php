74f5b05faeecfedede9f49dfa196d6d720995f9e
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-08-17 15:02:33 +0700

    add function index

--- application/controllers/admin/DataPegawai.php
+++ application/controllers/admin/DataPegawai.php
@@ -2,12 +2,27 @@
 
 class DataPegawai extends CI_Controller
 {
-    public function index() {
-        $data = $this->db->query("SELECT * FROM data_pegawai")->result();
+    // public function __construct()
+	// {
+	// 	parent::__construct();
+	// 	$this->load->model('M_penggajian', 'penggajian');
+	// }
 
-        echo "<pre>";
-        print_r($data);
-        echo "</pre>";
+    // public function _rules() {
+    //     $this->form_validation->set_rules('nama_jabatan', 'nama jabatan', 'required');
+    //     $this->form_validation->set_rules('gaji_pokok', 'gaji pokok', 'required');
+    //     $this->form_validation->set_rules('tj_transport', 'tunjangan transport', 'required');
+    //     $this->form_validation->set_rules('uang_makan', 'uang makan', 'required');
+    // }
+    
+    public function index() {
+        $data['title'] = 'Data Pegawai';
+        $data['pegawai'] = $this->penggajian->getData('data_pegawai')->result();
+
+        $this->load->view('templates_admin/header', $data);
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/data-pegawai', $data);
+        $this->load->view('templates_admin/footer');
     }
 }
 
