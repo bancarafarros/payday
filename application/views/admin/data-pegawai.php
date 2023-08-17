38b47a6f1fde38dcdf9b0b306f5fd1755afc2e0d
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-08-17 15:05:20 +0700

    add button tambah pegawai

--- application/views/admin/data-pegawai.php
+++ application/views/admin/data-pegawai.php
@@ -4,6 +4,7 @@
     <h1 class="h3 mb-0 text-gray-800"><?= $title ?></h1>
 </div>
 <?= $this->session->flashdata('alert'); ?>
+<a class="btn btn-sm btn-primary mb-3" href="<?= base_url('admin/DataPegawai/halamanTambahPegawai') ?>"><i class="fas fa-plus mr-2"></i>Tambah pegawai</a>
 </div>
 <!-- /.container-fluid -->
 
