d5c570d150854ccdee2a348bb6ad67478c3c5b86
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-08-17 16:09:34 +0700

    add div.form-group

--- application/views/admin/tambah-data-jabatan.php
+++ application/views/admin/tambah-data-jabatan.php
@@ -10,13 +10,19 @@
             <div class="form-group">
                 <label for="">Nama Jabatan</label>
                 <input type="text" name="nama_jabatan" class="form-control"><?= form_error('nama_jabatan', '<div class="text-small text-danger"></div>') ?>
+            </div>
 
+            <div class="form-group">
                 <label for="">Gaji Pokok</label>
                 <input type="text" name="gaji_pokok" class="form-control"><?= form_error('gaji_pokok', '<div class="text-small text-danger"></div>') ?>
+            </div>
 
+            <div class="form-group">
                 <label for="">Tunjangan Transport</label>
                 <input type="text" name="tj_transport" class="form-control"><?= form_error('tj_transport', '<div class="text-small text-danger"></div>') ?>
+            </div>
 
+            <div class="form-group">
                 <label for="">Uang Makan</label>
                 <input type="text" name="uang_makan" class="form-control"><?= form_error('uang_makan', '<div class="text-small text-danger"></div>') ?>
             </div>
