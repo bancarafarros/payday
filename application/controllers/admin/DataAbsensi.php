9a36128ffea835ab9bae268ff1b247bdb3f4a5fa
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-08-18 17:03:50 +0700

    add function index

--- application/controllers/admin/DataAbsensi.php
+++ application/controllers/admin/DataAbsensi.php
@@ -8,6 +8,14 @@ class DataAbsensi extends CI_Controller
 		$this->load->model('M_penggajian', 'penggajian');
 	}
 
+    public function index() {
+        $data['title'] = 'Data Absensi Pegawai';
+
+        $this->load->view('templates_admin/header', $data);
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/data-absensi', $data);
+        $this->load->view('templates_admin/footer');
+    }
 
 }
 
