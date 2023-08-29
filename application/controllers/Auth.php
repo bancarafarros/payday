fd00d005e8dcfff7b2c47834548e68879dc78e76
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2023-08-29 17:00:56 +0700

    finish login

---
+++ application/controllers/Auth.php
@@ -0,0 +1,58 @@
+<?php
+
+class Auth extends CI_Controller
+{
+    public function __construct() {
+		parent::__construct();
+		$this->load->model('M_penggajian', 'penggajian');
+	}
+
+    public function _rules() {
+        $this->form_validation->set_rules('username', 'username', 'required');
+        $this->form_validation->set_rules('password', 'password', 'required');
+    }
+
+    public function index() {
+        $data['title'] = 'Login';
+
+        $this->load->view('templates_admin/header', $data);
+        $this->load->view('login', $data);
+    }
+    
+    public function login() {
+        $this->_rules();
+
+        if ($this->form_validation->run() ==  FALSE) {
+            redirect('Auth');
+        
+        } else {
+            $username = $this->input->post('username');
+            $password = sha1($this->input->post('password'));
+
+            $auth = $this->penggajian->cekLogin($username, $password);
+
+            if ($auth == FALSE) {
+                $this->session->set_flashdata('alert', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>NGA DL XOB</strong><button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span></button></div>');
+                redirect('Auth');
+            
+            } else {
+                switch ($auth->role_id) {
+                    case 1:
+                        redirect('admin/Dashboard');
+                        break;
+
+                    case 2:
+                        redirect('pegawai/Dashboard');
+                        break;
+                    
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
+
+?>
--- application/models/M_penggajian.php
+++ application/models/M_penggajian.php
@@ -25,6 +25,22 @@ class M_penggajian extends CI_Model
             $this->db->insert_batch($table, $data);
         }
     }
+
+    public function cekLogin() {
+        $username = set_value('username');
+        $password = set_value('password');
+
+        $result = $this->db->where('username', $username)
+                            ->where('password', sha1($password))
+                            ->limit(1)
+                            ->get('data_pegawai');
+        
+        if ($result->num_rows() > 0) {
+            return $result->row();
+        } else {
+            return FALSE;
+        }
+    }
 }
 
 ?>
